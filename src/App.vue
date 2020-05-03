e407a248c44c44f6f6ee910e5f2aa7d05c188cef
awttt <350944364@qq.com>
2020-05-03 18:38:37 +0800

    导航栏“

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
-import router from './router/index';
 <template>
   <div>
     <router-view/>
-    
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  * {
+    margin: 0; padding: 0;
+    box-sizing: border-box;
+  }
+  body{
+    line-height: 1.5;
+  }
+  a{
+    text-decoration: none;
+    color: inherit;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2C3E50;
+  }
+  #nav {
+    padding: 30px;
+    a {
+      font-weight: bold;
+      color: #2C3E50;
+      &.router-link-exact-active {
+        color: #42B983;
+      }
     }
   }
-}
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,25 +1,47 @@
 <template>
-    <div>
-        <div>
-      <router-link to='/money'>
-      <svg>
-          <use xlink:href="icon_label"/>
-      </svg>
-      计账</router-link>|
-      <router-link to='/labels'>标签</router-link>|
-      <router-link to='/statistics'>统计</router-link>
-    </div>
-    </div>
+  <nav>
+    <router-link to="/money" class="item" active-class="selected">
+      <Icon name="money"/>
+      记账
+    </router-link>
+    <router-link to="/labels" class="item" active-class="selected">
+      <Icon name="icon_label"/>
+      标签
+    </router-link>
+    <router-link to="/statistics" class="item" active-class="selected">
+      <Icon name="line chart 3"/>
+      统计
+    </router-link>
+  </nav>
 </template>
 
 <script lang="ts">
-import x from '@/assets/icons/icon_label.svg'
-console.log(x);
+
     export default{
         name:'Nav'
     }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  nav {
+    display: flex;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+    flex-direction: row;
+    font-size: 12px;
+    > .item {
+      padding: 2px 0;
+      width: 33.33333%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      .icon {
+        width: 32px;
+        height: 32px;
+      }
+    }
+    > .item.selected{
+      color: red;
+    }
+  }
 </style>
