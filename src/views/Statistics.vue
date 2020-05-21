aa3f7daf6a6c7df44c747dc3c5703240097de35d
awttt <350944364@qq.com>
2020-05-21 13:00:44 +0800

    完成统计页面样式

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -3,26 +3,45 @@
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
-    <div>
-      type:{{type}}
-      <br/>
-      interval:{{interval}}
-    </div>
-
-    <div>
       <ol>
         <li v-for="(group,index) in result" :key="index">
-          <h3>{{group.title}}</h3>
+          <h3 class="title">{{group.title}}</h3>
           <ol>
-            <li v-for="item in group.items" :key="item.id">
-              {{item.amount}} {{item.createdAt}}
+            <li v-for="item in group.items" :key="item.id"
+            class="record">
+             <span>{{tagString(item.tags)}}</span>
+              <span class="notes">{{item.notes}}</span>
+              <span>￥{{item.amount}}</span>
             </li>
           </ol>
         </li>
       </ol>
-    </div>
   </Layout>
 </template>
+<style scoped lang="scss">
+  %item{
+    padding: 8px 16px;
+    line-height: 24px;
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+  }
+
+  .title{
+    @extend %item
+
+  }
+  .record{
+    background:#f2f2f2;
+    @extend %item
+  }
+
+  .notes{
+    margin-right: auto;
+    margin-left: 8px;
+    color: #999;
+  }
+</style>
 
 <script lang="ts">
 
@@ -34,6 +53,11 @@
 
   @Component({components: {Tabs}})
   export default class Statistics extends Vue {
+
+    tagString(tags: Tag[]) {
+      return tags.length === 0 ? '无' : tags.join(',');
+    }
+
 
     get recordList() {
       return (this.$store.state as RootState).recordList;
