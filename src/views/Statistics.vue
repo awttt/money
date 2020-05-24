626226e70a77f8980f38c7694cdf810eb946532f
awttt <350944364@qq.com>
2020-05-24 12:23:56 +0800

    解决手机样式问题和点击统计页面的收入出现的报错，对money页面进行了优化

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,45 +1,47 @@
 <template>
-
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
-    <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
-      <ol>
-        <li v-for="(group,index) in  groupedList" :key="index">
-          <h3 class="title">{{beautify(group.title)}}</h3>
-          <ol>
-            <li v-for="item in group.items" :key="item.id"
-            class="record">
-             <span>{{tagString(item.tags)}}</span>
-              <span class="staNotes">{{item.notes}}</span>
-              <span>￥{{item.amount}}</span>
-            </li>
-          </ol>
-        </li>
-      </ol>
+    <ol v-if="groupedList.length>0">
+      <li v-for="(group, index) in groupedList" :key="index">
+        <h3 class="title">{{beautify(group.title)}} <span>￥{{group.total}}</span></h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id"
+              class="record"
+          >
+            <span>{{tagString(item.tags)}}</span>
+            <span class="notes">{{item.notes}}</span>
+            <span>￥{{item.amount}} </span>
+          </li>
+        </ol>
+      </li>
+    </ol>
+    <div v-else class="noResult">
+      当前无记录
+    </div>
   </Layout>
 </template>
-
-
 <script lang="ts">
-
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
   import Tabs from '@/components/Tabs.vue';
   import recordTypeList from '@/constant/recordTypeList';
-  import intervalList from '@/constant/intervalList';
-  import dayjs from 'dayjs'
+  import dayjs from 'dayjs';
   import clone from '@/lib/clone';
-
-  @Component({components: {Tabs}})
+  @Component({
+    components: {Tabs},
+  })
   export default class Statistics extends Vue {
-
-    beautify(string: string){
-      const day = dayjs(string)
-      const now = dayjs()
-      if(day.isSame(now,'day')){
-        return '今天'
-      }else if (day.isSame(now.subtract(1,'day'),'day')){
-        return '昨天'
+    tagString(tags: Tag[]) {
+      return tags.length === 0 ? '无' : tags.map(t=>t.name).join(',');
+    }
+    beautify(string: string) {
+      const day = dayjs(string);
+      const now = dayjs();
+      if (day.isSame(now, 'day')) {
+        return '今天';
+      } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+        console.log('hi');
+        return '昨天';
       } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
         return '前天';
       } else if (day.isSame(now, 'year')) {
@@ -47,25 +49,19 @@
       } else {
         return day.format('YYYY年M月D日');
       }
-
     }
-
-
-    tagString(tags: Tag[]) {
-      return tags.length === 0 ? '无' : tags.join(',');
-    }
-
-
     get recordList() {
       return (this.$store.state as RootState).recordList;
     }
-
     get groupedList() {
       const {recordList} = this;
-      if (recordList.length === 0) {return [];}
+
       const newList = clone(recordList)
         .filter(r => r.type === this.type)
         .sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf());
+
+      if (newList.length === 0) {return [] as Result;}
+
       type Result = { title: string; total?: number; items: RecordItem[] }[]
       const result: Result = [{title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'), items: [newList[0]]}];
       for (let i = 1; i < newList.length; i++) {
@@ -86,20 +82,20 @@
       });
       return result;
     }
-
     beforeCreate() {
       this.$store.commit('fetchRecords');
     }
-
     type = '-';
-    interval = 'day';
     recordTypeList = recordTypeList;
-    intervalList = intervalList;
-
   }
 </script>
 
 <style scoped lang="scss">
+  .noResult{
+    padding: 16px;
+    text-align: center;
+  }
+
   ::v-deep .type-tabs-item {
     background: white;
     &.selected {
